d6a6a45f4280748823b774753cd7032b81e7f2c9
Lilia_Jemai <@DevHarmony>
2025-01-30 20:14:18 +0100

    Composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import TypesExample from './Components/Boutton'
+
+import Form from './Components/Form'
+import Hello from './Components/Hello'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +32,13 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+<Hello/>
+<Form labelle="FirstName" namee="firstName" />
+<Form labelle="SecondName" />
+<TypesExample />
+
     </>
   )
 }
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,27 @@
+export default function Hello () {
+    return ( 
+    <>
+        <h1>Hello Lilia</h1>
+    </> );
+}
+
+
+ 
+
+
+// export default Hello ;
+
+
+// import { Component } from "react";
+// class Hello extends Component {
+//     state = {  } 
+//     render() { 
+//         return (
+//             <div>
+//                  <h1>Hello 4 TWIN7</h1>
+//             </div> 
+//         );
+//     }
+// }
+ 
+// export default Hello;
